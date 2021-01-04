f2e22331958df4bbdbffc05e73ea4f3559e72f4b
Kit Langton <kit.langton@gmail.com>
2021-01-03 16:24:39 -0800

    Initial Commit

---
+++ OmenGridAnimation/OmenGridAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmenGridAnimationApp.swift
+//  OmenGridAnimation
+//
+//  Created by Kit Langton on 1/3/21.
+//
+
+import SwiftUI
+
+@main
+struct OmenGridAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
